bf9582ea304a981b02d978123df0e1485eac8680
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2024-03-14 15:54:12 -0400

    changes

--- lab7/reversi.c
+++ lab7/reversi.c
@@ -20,19 +20,31 @@ void printBoard(char board[][26], int n) {
             }
         }
     }
-    printf(" ");
+    printf("  ");
     for (int i = 0; i < n; i++) {
         printf("%c", coords[i]);
     }
     printf("\n");
-    for (int i = 0; i < n; i++) {
+    for (int i=0; i < n; i++) {
         printf("%c ", coords[i]);
         for (int j = 0; j < n; j++) {
             printf("%c", board[i][j]);
         }
         printf("\n");
     }
-    char color, row, col;
+}
+
+bool positionInBounds(int n, int row, int col) {
+    int b;
+}
+
+bool checkLegalInDirection(char board[][26], int n, int row, int col, char colour, int deltaRow, int deltaCol) {
+    int x;
+}
+
+void readBoard(char board[][26], int n) {
+char coords []={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+ char color, row, col;
     printf("Enter board configuration: \n");
     while (scanf(" %c%c%c", &color, &row, &col)==3&&color!= '!') {
         int i=row-'a';
@@ -56,21 +68,12 @@ void printBoard(char board[][26], int n) {
     }
 }
 
-bool positionInBounds(int n, int row, int col) {
-}
-
-bool checkLegalInDirection(char board[][26], int n, int row, int col,
-                           char colour, int deltaRow, int deltaCol) {
-}
-
-void readBoard(char board[][26], int n) {
-
-}
 int main(void) {
   int n; 
   printf("Enter board dimensions: ");
   scanf("%d", &n);
   char board[n][26];
   printBoard(board,n);
+  readBoard(board, n);
 
 }
